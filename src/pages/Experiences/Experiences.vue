8d429649f62424834da83e60217d77dabfd43390
CharllyG23 <62917491+CharllyG23@users.noreply.github.com>
2022-09-21 14:25:45 -0300

    fixes

--- src/pages/Experiences/Experiences.vue
+++ src/pages/Experiences/Experiences.vue
@@ -16,7 +16,6 @@
                   </div>
                </div>
             </div>
-
          </div>
       </div>
    </div>
@@ -25,19 +24,19 @@
 
 const steps = [
    {
-		title: 'PlugApps',
+		title: 'Plug Apps',
 		time: 'Outubro 2020 - Junho 2021',
-      subTitle: 'Codifiquei e mantive sites internos da empresa usando principalmente Html, Css, JavaScript , Vue, Laravel.',
+      subTitle: 'Colaborei muito de perto com a maioria da equipe codificando e corrigindo bugs em sites internos da empresa usando principalmente Vue e Laravel.',
 	},
    {
 		title: 'A&M Solutions',
 		time: 'Julho 2021 - setembro 2021',
-      subTitle: 'Conectado com desenvolvedores para garantir experiências de usuário satisfatórias e consistência de projeto que os clientes precisavam.',
+      subTitle: 'Trabalhei principalmente para garantir experiências de usuário satisfatórias e consistência de projeto que os clientes precisam no ReactJs e Bootstrap.',
 	},
    {
 		title: 'Partyou',
 		time: 'Novembro 2021 - 2022',
-      subTitle: 'Junto com a equipe de frontend, criamos código moderno, eficiente e de fácil manutenção com o framework vue 3 para uma ampla gama de projetos internos.',
+      subTitle: 'Junto com a equipe de frontend, criamos código moderno, eficiente e de fácil manutenção com o framework Vue 3 para uma ampla gama de projetos internos.',
 	},
 ]
 </script>
