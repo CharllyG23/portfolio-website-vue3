7b070592d7dc1e8fda41c43f78771d56462cbeba
CHARLLY GARCIA <charllyfiguera21@gmail.com>
2022-11-26 21:19:37 -0300

    animation

--- src/pages/cHero/cHero.vue
+++ src/pages/cHero/cHero.vue
@@ -1,19 +1,22 @@
 <template>
   <section id="home" class="pages">
     <div class="hero-section">
-      <div class="letter">
-        <h3>FRONT</h3>
-        <h3>END</h3>
-      </div>
+      <transition name="fade" mode="out-in">
+        <div v-if="display" class="letter">
+          <h3>FRONT</h3>
+          <h3>END</h3>
+        </div>
+      </transition>
       <div class="pages-wrapper">
         <div class="pages-wrapper-content">
-            <div class="content">
+          <transition  name="nested">
+            <div v-if="display" class="content">
               <div class="content-header">
                 <h1>Oiê</h1>
                 <app-icons name="hands" :size="34" class="ml-5"></app-icons>
               </div>
               <div class="content-coffee">
-                <h1>Sou Charlly,</h1>
+                <h1 >Sou Charlly,</h1>
                 <app-icons name="coffee" :size="80"></app-icons>
               </div>
                 <h2 class="content-degraded"><span class="underline">Frontend Developer</span></h2>
@@ -24,31 +27,44 @@
                 <a :href="Cv" download>Download CV<app-icons name="download" :size="24" class="ml-2" /></a>
               </div>
             </div>
+          </transition>
         </div> 
       </div>
     </div>
-    <div class="icons">
-      <ul>
-        <li>
-          <a :href="gitHubUrl" target="_blank"><app-icons name="git-hub" :size="28"></app-icons></a>
-        </li>
-        <li>
-          <a :href="linkedinUrl" target="_blank"><app-icons name="linkedin" :size="28"></app-icons></a>
-        </li>
-        <li>
-          <a :href="instagramUrl" target="_blank"><app-icons name="instagram" :size="28"></app-icons></a>
-        </li>
-      </ul>   
-    </div>
+    <transition name="slide-fade" >
+      <div v-if="display" class="icons">
+        <ul>
+          <li>
+            <a :href="gitHubUrl" target="_blank"><app-icons name="git-hub" :size="28"></app-icons></a>
+          </li>
+          <li>
+            <a :href="linkedinUrl" target="_blank"><app-icons name="linkedin" :size="28"></app-icons></a>
+          </li>
+          <li>
+            <a :href="instagramUrl" target="_blank"><app-icons name="instagram" :size="28"></app-icons></a>
+          </li>
+        </ul>   
+      </div>
+    </transition>  
   </section>
 </template>
 <script setup>
 import AppIcons from '../../components/AppIcons/AppIcons.vue';
-import Cv from '../../assets/file/CV-Charlly-Garcia.pdf'
+import Cv from '../../assets/file/CV-Charlly-Garcia.pdf';
+import { onMounted, ref } from 'vue';
 
 const instagramUrl = import.meta.env.VITE_APP_INSTAGRAM_URL
 const gitHubUrl = import.meta.env.VITE_APP_GITHUB_URL
 const linkedinUrl = import.meta.env.VITE_APP_LINKEDIN_URL
+
+const display = ref(false)
+
+onMounted(() =>{
+  display.value = false
+  setTimeout(() =>{
+    display.value = true
+  }, 500)
+})
 </script>
 <style lang="scss" scoped>
   @import './cHero-style.scss';
