0801d08476e0916e0e14345ee382d12c4f6ba699
CharllyG23 <charllyfiguera21@gmail.com>
2023-07-01 17:47:58 -0300

    Fix: style component

--- src/pages/cHero/cHero.vue
+++ src/pages/cHero/cHero.vue
@@ -13,12 +13,12 @@
             <div v-if="display" class="content">
               <div class="content-header">
                 <h1 class="pb-2">Oiê</h1>
-                <app-icons name="hands" :size="34" class="ml-5 hand"></app-icons>
+                <app-icons name="hands" :size="40" class="ml-5 hand"></app-icons>
               </div>
                 <h1 class="pb-3">Sou Charlly,</h1>
                 <h2 class="content-degraded"><span class="underline">{{ currentText }}</span></h2>
               <div class="content-subTitle">
-                <p>Engenheira em Informática, dedicada ao Desenvolvimento Front-end, com mais de <strong class="text-bold">3+</strong> anos de experiência.</p>
+                <p>Engenheira em Informática, dedicada ao Desenvolvimento Front-end, com mais de <strong class="text-lg">3+</strong> anos de experiência.</p>
               </div>
             </div>
           </transition>
@@ -51,13 +51,13 @@ const gitHubUrl = import.meta.env.VITE_APP_GITHUB_URL
 const linkedinUrl = import.meta.env.VITE_APP_LINKEDIN_URL
 
 const display = ref(false)
-const currentText = ref('Frontend Developer')
+const currentText = ref('FRONTEND DEVELOPER')
 
 onMounted(() =>{
   display.value = false
   setTimeout(() =>{
     display.value = true
-  }, 500)
+  }, 300)
 })
 </script>
 <style lang="scss" scoped>
